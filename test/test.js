3162be826a627d668a51dd7431d9f2e2948ddea0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-05-21 13:53:24 +0100

    structure

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const nearley = require("nearley");
+const grammar = require("./test-grammar.js");
+const { lexer } = require('../index.js');
+const util = require('util');
+const ins = obj => console.log(util.inspect(obj, { depth: null }));
+
+let s = `
+fun (id, idtwo, idthree)  
+  do  
+    do end;
+    do end
+  end 
+end`;
+let ans;
+try {
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+   // Parse something!
+    parser.feed(s);
+    ins(parser.results);
+} catch (e) {
+    console.log(e);
+}
+
---
+++ test/tokens.js
@@ -0,0 +1,52 @@
+const moo = require("moo");
+
+module.exports = {
+    ws: { match: /\s+|#[^\n]*/, lineBreaks: true },
+    lte: "<=",
+    lt: "<",
+    gte: ">=",
+    gt: ">",
+    eq: "==",
+    lparan: "(",
+    rparan: ")",
+    comma: ",",
+    lbracket: "[",
+    rbracket: "]",
+    lbrace: "{",
+    rbrace: "}",
+    assignment: "=",
+    plus: "+",
+    minus: "-",
+    multiply: "*",
+    divide: "/",
+    modulo: "%",
+    colon: ":",
+    semicolon: ";",
+    string_literal: {
+        match: /"(?:[^\n\\"]|\\["\\ntbfr])*"/,
+        value: s => JSON.parse(s)
+    },
+    number_literal: {
+        match: /[0-9]+(?:\.[0-9]+)?/,
+        value: s => Number(s)
+    },
+    identifier: {
+        match: /[a-z_][a-z_0-9]*/,
+        type: moo.keywords({
+            fun: "fun",
+            proc: "proc",
+            while: "while",
+            for: "for",
+            else: "else",
+            in: "in",
+            if: "if",
+            return: "return",
+            and: "and",
+            or: "or",
+            true: "true",
+            false: "false",
+            end: "end",
+            dolua: "do"
+        })
+    }
+}
