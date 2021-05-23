a1d877a4749f25521efef2b6c6a6ce66329a3a49
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-05-23 09:29:52 +0100

    first test implemented

--- test/test.js
+++ test/test.js
@@ -1,22 +1,38 @@
+const expect = require("chai").expect;
+
 const nearley = require("nearley");
 const grammar = require("./test-grammar.js");
 const util = require('util');
 const ins = obj => console.log(util.inspect(obj, { depth: null }));
 
-let s = `
+describe("Testing moo-ignore", function () {
+  it('test-grammar lexer.ignore("ws", "comment");', function (done) {
+    let s = `
 fun (id, idtwo, idthree)  
   do   #hello
     do end;
     do end # another comment
   end 
 end`;
-let ans;
-try {
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-   // Parse something!
-    parser.feed(s);
-    ins(parser.results);
-} catch (e) {
-    console.log(e);
-}
+    let ans;
+    try {
+        const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+      // Parse something!
+        parser.feed(s);
+        expect(parser.results[0]).deep.equal([
+          'fun',        'lp',
+          'identifier', 'comma',
+          'identifier', 'comma',
+          'identifier', 'rp',
+          'dolua',      'dolua',
+          'end',        'semicolon',
+          'dolua',      'end',
+          'end',        'end'
+        ]);
+        done();
+    } catch (e) {
+        console.log(e);
+    }
+  });
+});
 
