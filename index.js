55c54daa5add44c999a586993b7cca1d90ec620c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-05-24 11:14:01 +0100

    covering added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const moo = require("moo");
 
 // Static Method
@@ -13,40 +15,33 @@ function makeLexer(tokens, ignoreTokens) {
   let lexer; 
   let oldnext; 
 
-  try {
+  
     lexer = moo.compile(tokens);
     oldnext = lexer.next;
   
     lexer.ignore = function(...types) {
-      if (this.ignoreSet) {
-        this.ignoreSet = new Set([...this.ignoreSet, ...types]);
+      if (this.ignoreSet) { // ignoreSet was built at construction time
+        this.ignoreSet = new Set([...this.ignoreSet, ...types]); // union
       }
-      else  this.ignoreSet = new Set(types);
+      else  this.ignoreSet = new Set(types); // build it now
     }
-    
-    lexer.next = function () {
-      try {
+ 
+    lexer.next = function () {    
         while (true) {
           let token = oldnext.call(this);
+          // moo oldnext iterator returns undefined when finished
           if (token == undefined || !this.ignoreSet.has(token.type)) {
             return token;
           } 
           //console.error("ignoring token "+token.type);
         }
-      } catch (e) {
-          console.error("Eh!\n" + e)
-      }
     };
   
     if (ignoreTokens) {
       lexer.ignoreSet = new Set(ignoreTokens);
     }
-
     return lexer;
-  } catch (e) {
-    throw new Error(`Error in moo-ignore inside makeLexer:\n${e}`);
-  }
-
+   
 }
 
 module.exports = {makeLexer, moo};
